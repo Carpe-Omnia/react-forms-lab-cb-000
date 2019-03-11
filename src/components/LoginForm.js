4c460620990f14aefe80d5c529d67cbab25be591
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-11 19:43:41 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,20 +9,29 @@ class LoginForm extends React.Component {
       password: ""
   };
   }
-
+  handleText = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+  handleWord = event => {
+    this.setState({
+      password: event.target.value
+    })
+  }
   render() {
     return (
       <form>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={event => this.handleText(event)}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={event => this.handleWord(event)}/>
           </label>
         </div>
         <div>
